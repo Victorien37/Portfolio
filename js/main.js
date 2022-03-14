e9dcd20d9597daa9dbdc56e74766dacc50a21397
Victorien <rodriguesvictorien37@gmail.com>
2022-03-14 13:37:32 +0100

    Modification de la partie "a propos" dans index.html; ajout de plusieurs images pour illustrer le portfolio et ajout d'un repertoire pdf contenant mon CV.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-    //OVERLAY PRESENTATION
-    $("#open_overlay1").click(function(){
-        $("#overlay1").css({
-            "visibility" : "visible"
-        });
-    });
-    $("#close_overlay1").click(function(){
-        $("#overlay1").css({
-            "visibility" : "hidden"
-        });
-    });
     /////////////////////////////////////
     //OVERLAY SLAM
     $("#open_overlay_slam").click(function(){
@@ -168,7 +157,6 @@ $(document).ready(function(){
         });
     });
     //////////////////////////////////////////
-    //const svg = document.querySelector("svg");
     //ACCORDEON PARCOURS
     $(".accordeon-content").click(function(){
         $(".accordeon").css({
@@ -181,22 +169,14 @@ $(document).ready(function(){
                 "max-height" : "0",
                 "border-color" : "#343A40"
             });
-            //var test = document.getElementsByClassName("open").getAttribute('d');
-            /*if (document.getElementsByClassName("open").getAttribute('d')) {
-                svg.innerHTML = svg.innerHTML.replace(
-                    "M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z",
-                    "M.172 15.828a.5.5 0 0 0 .707 0l4.096-4.096V14.5a.5.5 0 1 0 1 0v-3.975a.5.5 0 0 0-.5-.5H1.5a.5.5 0 0 0 0 1h2.768L.172 15.121a.5.5 0 0 0 0 .707zM15.828.172a.5.5 0 0 0-.707 0l-4.096 4.096V1.5a.5.5 0 1 0-1 0v3.975a.5.5 0 0 0 .5.5H14.5a.5.5 0 0 0 0-1h-2.768L15.828.879a.5.5 0 0 0 0-.707z"
-                    );
-                
-            }*/
-
-            
+            // $(this).document.querySelector(".open").setAttributeNS("http://www.w3.org/2000/svg", "d", "M.172 15.828a.5.5 0 0 0 .707 0l4.096-4.096V14.5a.5.5 0 1 0 1 0v-3.975a.5.5 0 0 0-.5-.5H1.5a.5.5 0 0 0 0 1h2.768L.172 15.121a.5.5 0 0 0 0 .707zM15.828.172a.5.5 0 0 0-.707 0l-4.096 4.096V1.5a.5.5 0 1 0-1 0v3.975a.5.5 0 0 0 .5.5H14.5a.5.5 0 0 0 0-1h-2.768L15.828.879a.5.5 0 0 0 0-.707z");
         } // FERMETURE DE L'ACCORDEON
         else {
             $(this).children(".accordeon").css({
                 "max-height" : "2000px",
                 "border-color" : "#FFC107"
             });
+            // $(this).document.querySelector(".open").setAttributeNS("http://www.w3.org/2000/svg", "d", "M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z");
         }
     });
     /////////////////////////////////////////////////
